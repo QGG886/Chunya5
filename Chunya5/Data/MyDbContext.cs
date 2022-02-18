e0c824846e042943f4bc199f5deb9cb9d16897ee
zeq886 <zeq886@outlook.com>
2022-02-18 20:10:10 +0800

    init

---
+++ Chunya5/Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chunya5.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Chunya5/Program.cs
@@ -0,0 +1,31 @@
+using Chunya5.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContextPool<MyDbContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("mysqlDbconn"),
+        new MySqlServerVersion(new Version(8, 0, 26)));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
